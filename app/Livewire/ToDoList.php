4e5db416d2521bef12ee21318e1548ba189a01db
Ahryma <arianur098@gmail.com>
2024-01-23 11:41:01 +0800

    Create Feature

---
+++ app/Livewire/ToDoList.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Todo;
+use Livewire\Component;
+use Livewire\Attributes\Rule;
+
+class ToDoList extends Component
+{
+    // Rule for Name
+    #[Rule('required|min:3|max:50')]
+    public $name;
+
+    public $search;
+
+    public function create()
+    {
+        /* Steps :
+        1. Validate
+        2. Create the To Do List
+        3. Clear inputs
+        4. Send Flash Message
+        */
+
+        // First Step
+        $validated = $this->validateOnly('name');
+
+        // Second Step
+        Todo::create($validated);
+
+        // Third Step
+        $this->reset('name');
+
+        // Forth Step
+        session()->flash('success', 'Successfully Created !');
+    }
+
+
+    public function render()
+    {
+        return view('livewire.to-do-list');
+    }
+}
